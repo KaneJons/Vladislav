874b4c28f73ee80d6a285a1fee938b666d575ba6
strelnikov <strelnikov7953@gmail.com>
2024-12-18 17:33:54 +0300

    исправлен 5.2

--- Lab5.2/Lab5.2.cpp
+++ Lab5.2/Lab5.2.cpp
@@ -1,61 +1,7 @@
-﻿#include <iostream> // Подключение библиотеки для работы с вводом и выводом (cout, cin, endl).
-#include <malloc.h> // Подключение библиотеки для работы с функциями выделения памяти (calloc, free).
+﻿#include <iostream>
+#include <malloc.h>
 
-using namespace std; // Использование пространства имён std для упрощения доступа к стандартным функциям.
-
-// Функция для вывода матрицы.
-void printMatrix(int** matrix, int rows, int cols) {
-    // Цикл по строкам матрицы.
-    for (int i = 0; i < rows; i++) {
-        // Цикл по столбцам матрицы.
-        for (int j = 0; j < cols; j++) {
-            cout << matrix[i][j] << "\t"; // Вывод элемента матрицы с табуляцией для форматирования.
-        }
-        cout << endl; // Переход на следующую строку после вывода всех элементов строки.
-    }
-}
-
-// Функция обработки матрицы.
-void processMatrix(int** matrix, int rows, int cols) {
-    
-    // Переменные для подсчёта количества положительных, отрицательных и нулевых элементов.
-    int posCount = 0, negCount = 0, zeroCount = 0, temp;
-
-    // Проход по каждому элементу матрицы.
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-
-            // Подсчёт положительных, отрицательных и нулевых элементов.
-            if (matrix[i][j] > 0) posCount++;
-            else if (matrix[i][j] < 0) negCount++;
-            else zeroCount++;
-            
-            if (matrix[i][j] > matrix[0][0]) //нахождение максимального элемента с сменой первого элемента
-            {
-                temp = matrix[0][0];
-                matrix[0][0] = matrix[i][j];
-                matrix[i][j] = temp;
-            }
-
-            if (matrix[i][j] < matrix[rows - 1][cols - 1])//нахождение минимального элемента со сменой последеним
-            {
-                temp = matrix[rows - 1][cols - 1];
-                matrix[rows - 1][cols -1] = matrix[i][j];
-                matrix[i][j] = temp;
-            }
-           
-        }
-    }
-
-    // Вывод обработанной матрицы.
-    cout << "Обработанная матрица:\n";
-    printMatrix(matrix, rows, cols);
-
-    // Вывод количества положительных, отрицательных и нулевых элементов.
-    cout << "Количество положительных элементов: " << posCount << endl;
-    cout << "Количество отрицательных элементов: " << negCount << endl;
-    cout << "Количество нулевых элементов: " << zeroCount << endl;
-}
+using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian"); // Установка локализации для работы с русским языком в консоли.
@@ -78,13 +24,57 @@ int main() {
             cin >> matrix[i][j]; // Ввод каждого элемента матрицы.
         }
     }
+    // Переменные для подсчёта количества положительных, отрицательных и нулевых элементов.
+
+    int posCount = 0, negCount = 0, zeroCount = 0;
 
     // Вывод исходной матрицы.
     cout << "Исходная матрица:\n";
-    printMatrix(matrix, rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
 
-    // Обработка матрицы.
-    processMatrix(matrix, rows, cols);
+            if (matrix[i][j] > 0) posCount++;
+            else if (matrix[i][j] < 0) negCount++;
+            else zeroCount++;
+        }
+        cout << endl;
+    }
+
+    int  temp;
+
+    // Проход по каждому элементу матрицы и обработка.
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+           
+
+            if (matrix[i][j] > matrix[0][0]) { // Нахождение максимального элемента с заменой первого элемента.
+                temp = matrix[0][0];
+                matrix[0][0] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+
+            if (matrix[i][j] < matrix[rows - 1][cols - 1]) { // Нахождение минимального элемента с заменой последнего.
+                temp = matrix[rows - 1][cols - 1];
+                matrix[rows - 1][cols - 1] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+
+    // Вывод обработанной матрицы.
+    cout << "Обработанная матрица:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    // Вывод количества положительных, отрицательных и нулевых элементов.
+    cout << "Количество положительных элементов: " << posCount << endl;
+    cout << "Количество отрицательных элементов: " << negCount << endl;
+    cout << "Количество нулевых элементов: " << zeroCount << endl;
 
     // Освобождение памяти, выделенной для матрицы.
     for (int i = 0; i < rows; i++) {
